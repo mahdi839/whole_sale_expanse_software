67ea5ee061ffc3a632185b98c61db80654b9bd65
mahdi839 <hasanarefi56574@gmail.com>
2026-04-19 06:51:44 +0600

    set app name and fav icon and app title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('inaya_creation_logo.jpeg') }}">
+    <title>{{ config('app.name', 'Inaya Creation') }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
